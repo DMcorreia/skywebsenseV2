12fa3bb7e39d0d11e4f7debe207f0b63c4cc4d5d
Daniel Correia <Daniel.Correia@tmg.pt>
2018-05-06 17:40:28 +0100

    base

--- sky-websense.c
+++ sky-websense.c
@@ -1,7 +1,8 @@
 
+
 #include "contiki.h"
 #include "httpd-simple.h"
-#include "dev/sht11/sht11-sensor.h"
+#include "dev/sht11-sensor.h"
 #include "dev/light-sensor.h"
 #include "dev/leds.h"
 #include <stdio.h>
@@ -26,13 +27,21 @@ AUTOSTART_PROCESSES(&web_sense_process,&webserver_nogui_process);
 #define HISTORY 16
 static int temperature[HISTORY];
 static int light1[HISTORY];
-static int sensors_pos;
 
 /*---------------------------------------------------------------------------*/
 static int
 get_light(void)
 {
   return 10 * light_sensor.value(LIGHT_SENSOR_PHOTOSYNTHETIC) / 7;
+}
+
+static int
+get_hum(void)
+{
+ /*- return (((0.0405*sht11_sensor.value(SHT11_SENSOR_HUMIDITY)) - 4) + ((-2.8 * 0.000001)*(sht11_sensor.value(SHT11_SENSOR_HUMIDITY)))); --*/
+static int dec;  
+dec = (((0.0405*sht11_sensor.value(SHT11_SENSOR_HUMIDITY)) - 4) + ((-2.8 * 0.000001)*(sht11_sensor.value(SHT11_SENSOR_HUMIDITY))));
+return dec;
 }
 /*---------------------------------------------------------------------------*/
 static int
@@ -41,7 +50,7 @@ get_temp(void)
   return ((sht11_sensor.value(SHT11_SENSOR_TEMP) / 10) - 396) / 10;
 }
 /*---------------------------------------------------------------------------*/
-static const char *TOP = "<html><head><title>Contiki Web Sense</title></head><body>\n";
+static const char *TOP = "<html><head><title>Sens</title></head><body>\n";
 static const char *BOTTOM = "</body></html>\n";
 /*---------------------------------------------------------------------------*/
 /* Only one single request at time */
@@ -50,21 +59,7 @@ static int blen;
 #define ADD(...) do {                                                   \
     blen += snprintf(&buf[blen], sizeof(buf) - blen, __VA_ARGS__);      \
   } while(0)
-static void
-generate_chart(const char *title, const char *unit, int min, int max, int *values)
-{
-  int i;
-  blen = 0;
-  ADD("<h1>%s</h1>\n"
-      "<img src=\"http://chart.apis.google.com/chart?"
-      "cht=lc&chs=400x300&chxt=x,x,y,y&chxp=1,50|3,50&"
-      "chxr=2,%d,%d|0,0,30&chds=%d,%d&chxl=1:|Time|3:|%s&chd=t:",
-      title, min, max, min, max, unit);
-  for(i = 0; i < HISTORY; i++) {
-    ADD("%s%d", i > 0 ? "," : "", values[(sensors_pos + i) % HISTORY]);
-  }
-  ADD("\">");
-}
+
 static
 PT_THREAD(send_values(struct httpd_state *s))
 {
@@ -74,35 +69,14 @@ PT_THREAD(send_values(struct httpd_state *s))
 
   if(strncmp(s->filename, "/index", 6) == 0 ||
      s->filename[1] == '\0') {
-    /* Default page: show latest sensor values as text (does not
-       require Internet connection to Google for charts). */
+
     blen = 0;
-    ADD("<h1>Current readings</h1>\n"
-        "Light: %u<br>"
-        "Temperature: %u&deg; C",
-        get_light(), get_temp());
+    ADD("L: %u "",T: %u&deg;C,""H: %u",
+        get_light(), get_temp(),get_hum());
     SEND_STRING(&s->sout, buf);
 
-  } else if(s->filename[1] == '0') {
-    /* Turn off leds */
-    leds_off(LEDS_ALL);
-    SEND_STRING(&s->sout, "Turned off leds!");
-
-  } else if(s->filename[1] == '1') {
-    /* Turn on leds */
-    leds_on(LEDS_ALL);
-    SEND_STRING(&s->sout, "Turned on leds!");
-
-  } else {
-    if(s->filename[1] != 't') {
-      generate_chart("Light", "Light", 0, 500, light1);
-      SEND_STRING(&s->sout, buf);
-    }
-    if(s->filename[1] != 'l') {
-      generate_chart("Temperature", "Celsius", 15, 50, temperature);
-      SEND_STRING(&s->sout, buf);
-    }
-  }
+  } 
+  
 
   SEND_STRING(&s->sout, BOTTOM);
 
@@ -120,20 +94,11 @@ PROCESS_THREAD(web_sense_process, ev, data)
   static struct etimer timer;
   PROCESS_BEGIN();
 
-  sensors_pos = 0;
-
   etimer_set(&timer, CLOCK_SECOND * 2);
   SENSORS_ACTIVATE(light_sensor);
   SENSORS_ACTIVATE(sht11_sensor);
 
-  while(1) {
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-    etimer_reset(&timer);
-
-    light1[sensors_pos] = get_light();;
-    temperature[sensors_pos] = get_temp();
-    sensors_pos = (sensors_pos + 1) % HISTORY;
-  }
+ 
 
   PROCESS_END();
 }
